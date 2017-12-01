2c1ff42c45d7caff1475e60e7f8009f77f08c785
z <zyn1573@163.com>
2017-12-01 13:02:30 +0800

    zz first

---
+++ app/src/main/java/com/zzcoolweather/android/AzzMain.java
@@ -0,0 +1,13 @@
+package com.zzcoolweather.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AzzMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.zzlayout_azz_main);
+    }
+}
